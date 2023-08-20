b5751da61d7ae4f48e5ab051158fe82577dcbfcf
Jason Alexander <jasonalexander.dev@gmail.com>
2023-08-20 23:41:49 +0100

    Initial commit.

---
+++ src/args.rs
@@ -0,0 +1,111 @@
+use std::path::PathBuf;
+use std::num::ParseIntError;
+use clap::{Parser, Subcommand, Args, ValueEnum};
+use baby_emulator::core::MEMORY_WORDS;
+
+
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+pub struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+pub enum Commands {
+    /// Assemble an asm source file to a binary file. 
+    Assemble(Assemble),
+    /// Load and run a source file. 
+    Run(Run)
+}
+
+#[derive(Args)]
+pub struct Assemble {
+    /// The input asm file. 
+    #[arg(long, short)]
+    pub input: PathBuf,
+
+    /// The output binary dump. 
+    #[arg(long, short)]
+    pub output: Option<PathBuf>,
+
+    /// Use original notation for asm instructions. 
+    #[arg(long, default_value_t = false)]
+    pub og_notation: bool,
+}
+
+#[derive(Args)]
+pub struct Run {
+    /// The source file to execute from. 
+    pub src: PathBuf,
+
+    /// The format of the file to execute from. 
+    #[arg(long, default_value_t = ExecuteFrom::Bin, value_enum)]
+    pub exe_from: ExecuteFrom,
+
+    /// Use original notation for asm instructions if running from asm. 
+    #[arg(long, default_value_t = false)]
+    pub og_notation: bool,
+    
+    /// Output whole `model` including registers & memory when execution stops or breakpoint encountered. 
+    #[arg(long, default_value_t = false)]
+    pub output_model: bool,
+
+    /// Memory addresses to output when execution stops or breakpoint encountered. 
+    #[arg(long, value_parser = parse_output_addresses)]
+    pub output_addr: Vec<usize>,
+
+    /// Registers to output when execution stops or breakpoint encountered. 
+    #[arg(long, value_enum)]
+    output_regs: Vec<Registers>,
+
+    /// Addresses where to break & output the state of the core. 
+    #[arg(long, value_parser = parse_breakpoint_addresses)]
+    pub break_addr: Vec<usize>,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+pub enum ExecuteFrom {
+    /// Executes from an assembly file. 
+    Asm,
+    /// Executes from a binary dump. 
+    Bin,
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Registers {
+    /// The accumulator register. 
+    Accumulator,
+    /// The instruction register. 
+    Instruction,
+    /// The instruction address register. 
+    InstructionAddress,
+}
+
+fn parse_output_addresses(input: &str) -> Result<usize, String> {
+    let res = parse_memory_address(input)
+        .map_err(|_| format!("Invalid value passed as output memory address `{input}`. "))?;
+    if res > MEMORY_WORDS {
+        return Err(format!("Value passed output memory address: {res}, Baby memory address space is {MEMORY_WORDS}. "));
+    }
+    Ok(res)
+}
+
+fn parse_breakpoint_addresses(input: &str) -> Result<usize, String> {
+    let res = parse_memory_address(input)
+        .map_err(|_| format!("Invalid value passed as breakpoint address `{input}`. "))?;
+    if res > MEMORY_WORDS {
+        return Err(format!("Value passed as breakpoint address: {res}, Baby memory address space is {MEMORY_WORDS}. "));
+    }
+    Ok(res)
+}
+
+fn parse_memory_address(value: &str) -> Result<usize, ParseIntError> {
+    let parse_res = match value {
+        v if v.starts_with("0x") => usize::from_str_radix(&v.replace("0x", ""), 16),
+        v if v.starts_with("0o") => usize::from_str_radix(&v.replace("0o", ""), 8),
+        v if v.starts_with("0b") => usize::from_str_radix(&v.replace("0b", ""), 2),
+        v => usize::from_str_radix(&v.replace("0d", ""), 10),
+    }?;
+    Ok(parse_res)
+}
---
+++ src/main.rs
@@ -0,0 +1,12 @@
+use std::path::PathBuf;
+use clap::{Parser, Subcommand};
+use args::Cli;
+
+pub mod args;
+
+
+fn main() {
+    let cli = Cli::parse();
+    
+    
+}
